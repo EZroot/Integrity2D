5c918eb27eeffbd2167444b27955af75ee57763d
ezroot <utohaha@gmail.com>
2025-12-02 00:16:11 -0330

    removed unused services from engine.cs

--- src/core/Engine.cs
+++ src/core/Engine.cs
@@ -17,15 +17,12 @@ public class Engine
 
     private readonly IGame m_Game;
     private readonly IEngineSettings m_Settings;
-    private readonly IAssetManager m_AssetManager;
     private readonly IInputManager m_InputManager;
     private readonly IWindowPipeline m_WindowPipe;
     private readonly IRenderPipeline m_RenderPipe;
     private readonly IImGuiPipeline m_ImGuiPipe;
     private readonly ISceneManager m_SceneManager;
-    private readonly IAudioManager m_AudioManager;
     private readonly ICameraManager m_CameraManager;
-    private readonly IGameObjectFactory m_GameObjectFactory;
     private readonly IProfiler m_Profiler;
 
     private readonly Dictionary<GLTexture, List<Matrix4x4>> m_RenderingBatchMap;
@@ -46,15 +43,12 @@ public class Engine
         m_Stopwatch = new Stopwatch();
 
         m_Settings = Service.Get<IEngineSettings>() ?? throw new Exception("Engine Settings service not found.");
-        m_AssetManager = Service.Get<IAssetManager>() ?? throw new Exception("Asset Manager service not found.");
         m_InputManager = Service.Get<IInputManager>() ?? throw new Exception("Input Manager service not found.");
         m_WindowPipe = Service.Get<IWindowPipeline>() ?? throw new Exception("Window Pipeline service not found.");
         m_RenderPipe = Service.Get<IRenderPipeline>() ?? throw new Exception("Render Pipeline service not found.");
         m_ImGuiPipe = Service.Get<IImGuiPipeline>() ?? throw new Exception("ImGui Pipeline service not found.");
         m_SceneManager = Service.Get<ISceneManager>() ?? throw new Exception("Scene Manager service not found.");
-        m_AudioManager = Service.Get<IAudioManager>() ?? throw new Exception("Audio Manager service not found.");
         m_CameraManager = Service.Get<ICameraManager>() ?? throw new Exception("Camera Manager service not found.");
-        m_GameObjectFactory = Service.Get<IGameObjectFactory>() ?? throw new Exception("GameObjectFactory service not found.");
         m_Profiler = Service.Get<IProfiler>() ?? throw new Exception("Profiler service not found.");
 
         m_Game = game;
